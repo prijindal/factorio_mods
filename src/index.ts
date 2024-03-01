3d8b93c9589a050f0d740b7f04c85468664f3fc8
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2024-03-01 23:18:01 +0530

    Fixed archive script

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import archiver from "archiver";
 import path from "path";
 import Yargs from "yargs";
 
-const archiveFolder = (sourceDir: string, outPath: string) => {
-  console.log(sourceDir, outPath);
+const archiveFolder = (
+  sourceDir: string,
+  mod_name: string,
+  outPath: string
+) => {
   const archive = archiver("zip", { zlib: { level: 9 } });
   const stream = fs.createWriteStream(outPath);
 
   return new Promise<void>((resolve, reject) => {
     archive
-      .directory(sourceDir, false)
+      .directory(sourceDir, mod_name)
       .on("error", (err) => reject(err))
       .pipe(stream);
 
@@ -19,26 +22,20 @@ const archiveFolder = (sourceDir: string, outPath: string) => {
   });
 };
 
+const MODS_FOLDER = path.join(process.cwd(), "mods");
+const ARCHIVED_PATH = path.join(process.cwd(), "archived");
+
 const argv = Yargs(process.argv.slice(2))
   .help()
-  .command(
-    "upload",
-    "Create an archive and upload to mods.factorio.com",
-    async () => {
-      console.log(process.cwd());
-      const MODS_FOLDER = path.join(process.cwd(), "mods");
-      const ARCHIVED_PATH = path.join(process.cwd(), "archived");
-      const mods = fs.readdirSync(MODS_FOLDER);
-      if (!fs.existsSync(ARCHIVED_PATH)) {
-        fs.mkdirSync(ARCHIVED_PATH);
-      }
-      console.log(mods);
-      for (const mod of mods) {
-        await archiveFolder(
-          path.join(MODS_FOLDER, mod),
-          path.join(ARCHIVED_PATH, `${mod}.zip`)
-        );
-      }
+  .command("archive", "Create an archive", async () => {
+    const mods = fs.readdirSync(MODS_FOLDER);
+    if (!fs.existsSync(ARCHIVED_PATH)) {
+      fs.mkdirSync(ARCHIVED_PATH);
     }
-  )
+    for (const mod of mods) {
+      const archivePath = path.join(ARCHIVED_PATH, `${mod}.zip`);
+      await archiveFolder(path.join(MODS_FOLDER, mod), mod, archivePath);
+      console.log(archivePath);
+    }
+  })
   .strict().argv;
